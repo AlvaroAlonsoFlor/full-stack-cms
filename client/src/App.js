83d7bd17f609bf89d7329722d913a0f2a3fc419b
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-04 20:47:34 +0000

    Create react client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h1>App working</h1>
+    );
+  }
+}
+
+export default App;
