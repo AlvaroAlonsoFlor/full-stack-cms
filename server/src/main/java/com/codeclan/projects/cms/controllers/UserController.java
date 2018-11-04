a6429a0bdb74654d77d49ef325ba6d9ce0710ada
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-04 19:07:59 +0000

    Add UserController and override user/id/articles route to display articles ordered

--- server/src/main/java/com/codeclan/projects/cms/controllers/UserController.java
+++ server/src/main/java/com/codeclan/projects/cms/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.codeclan.projects.cms.controllers;
 
+import com.codeclan.projects.cms.models.Article;
+import com.codeclan.projects.cms.repositories.UserRepository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/{id}/articles")
+    public List<Article> getUserArticlesSorted(@PathVariable Long id) {
+        return userRepository.getUserArticlesSortedByDate(id);
+    }
 }
