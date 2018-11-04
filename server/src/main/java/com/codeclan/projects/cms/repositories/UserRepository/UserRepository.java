03ef1605a39d948cbac0ab1f8a0d7926ae731d0c
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-04 16:24:15 +0000

    add embed articles to user repo

--- server/src/main/java/com/codeclan/projects/cms/repositories/UserRepository/UserRepository.java
+++ server/src/main/java/com/codeclan/projects/cms/repositories/UserRepository/UserRepository.java
@@ -2,8 +2,11 @@ package com.codeclan.projects.cms.repositories.UserRepository;
 
 import com.codeclan.projects.cms.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import projections.EmbedArticle;
+import projections.EmbedUser;
 
-@Repository
+@RepositoryRestResource(excerptProjection = EmbedArticle.class)
 public interface UserRepository extends JpaRepository<User, Long> {
 }
