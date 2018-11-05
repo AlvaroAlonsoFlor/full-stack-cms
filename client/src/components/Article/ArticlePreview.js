4782fb5f93551dca2e704a5c25f8d8e831e159c2
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-05 10:58:47 +0000

    article previews rendering to article container

--- client/src/components/Article/ArticlePreview.js
+++ client/src/components/Article/ArticlePreview.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const ArticlePreview = ({articles}) => {
-  console.log(articles)
+  const articlePreviews = articles.map((article) => {
+      return <div key = {article.id}>
+           <h2>{article.title}</h2>
+           <h4>{article.lead}</h4>
+           <h3>by {article.user.name}</h3>
+      </div>
+  })
 
   return(
-      <h2>Article Preview</h2>
+      <div>
+      <h1>Articles</h1>
+      {articlePreviews}
+      </div>
   )
 }
 
