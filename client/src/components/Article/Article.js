b13c78f1ea004bcf1c40a887579e8e4af8168e28
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-05 11:32:34 +0000

    article rendering created

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const Article = () => {
+const Article = ({article}) => {
+    return (
+        <div>
+        <h2>{article.title}</h2>
+        <h3>{article.lead}</h3>
+        <h4>{article.user.name}</h4>
+        <p>{article.body}</p>
+        </div>
+
+    )
 
 }
 
