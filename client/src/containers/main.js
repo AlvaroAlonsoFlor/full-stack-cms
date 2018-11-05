23b7cd4d7e94262fa00ae852670eadcf67b24607
Fofiko <foteinid@gmail>
2018-11-05 11:58:18 +0000

    Add initial User Nav Bar

--- client/src/containers/main.js
+++ client/src/containers/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
-import UserContainer from './UserContainer';
+import UserContainer from './Users/UserContainer';
 import ArticlesContainer from './ArticlesContainer';
 
 export default class Main extends Component {
