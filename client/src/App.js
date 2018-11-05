ea6323dd3fdc2e6ca0d77e47aaaa5a7c46d014f8
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-05 14:23:17 +0000

    line breaks handled for article body

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+
 import Main from './containers/Main.js';
 
 class App extends Component {
--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import '../../Article.css';
 
 const Article = (props) => {
    
     const thisArticle = props.location.state.article.article;
+    const splitPara = thisArticle.body.split('\n').map((item, key) => {
+        return <React.Fragment key={key}>{item}<br/><br/></React.Fragment>
+      })
     return (
         <div>
         <h2>{thisArticle.title}</h2>
         <h3>{thisArticle.lead}</h3>
         <h4>{thisArticle.user.name}</h4>
-        <p>{thisArticle.body}</p>
+        <p className = "article-body">{splitPara}</p>
         </div>
 
     )
