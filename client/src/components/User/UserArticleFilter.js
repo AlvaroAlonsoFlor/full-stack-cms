0deb728ee18ee5fe4570f152d407d40272b7ca1f
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-06 22:24:30 +0000

    Add change filter selected in UserArticleFilter, populate dropdown

--- client/src/components/User/UserArticleFilter.js
+++ client/src/components/User/UserArticleFilter.js
@@ -4,31 +4,51 @@ export default class UserArticleFilter extends Component {
      constructor(props) {
              super(props)
              this.state = {
-                 filterTag: 'all'
+                 articles: null,
+                 filterTag: 'all',
              };
     
-        this.handleTagChange = this.handleTagChange.bind(this);        
+        this.handleTagChange = this.handleTagChange.bind(this);
+        this.filterTagsForSelectMenu = this.filterTagsForSelectMenu.bind(this);        
+    }
+
+    filterTagsForSelectMenu() {
+        if (!this.props.articles) {
+            return;
+        }
+
+        const tags = [];
+        this.props.articles.map((article) => {
+
+            if (!tags.includes(article.tag)) {
+                return tags.push(article.tag)
+            }
+            return null;
+        })
+        return tags.map((tag, index) => {
+            const uppercaseTag = tag.charAt(0).toUpperCase() + tag.slice(1); 
+            return <option key = {index} value={tag}>{uppercaseTag}</option>
+        })
     }
 
     handleTagChange(event) {
-        this.setState({filterName: event.target.value}) 
+        this.setState({filterTag: event.target.value}) 
     }
 
     render () {
-     
-        
-        // const tagOptions = this.props.tags.map((tag, index) => {
-        //     const uppercaseTag = tag.charAt(0).toUpperCase() + tag.slice(1); 
-        //     return <option key = {index} value={tag}>{uppercaseTag}</option>
-        // })
-        
+        // this.filterTagsForSelectMenu()
+        console.log(this.state.filterTag);
+        if (!this.props.articles) {
+            return null
+        }
+        const tagOptions = this.filterTagsForSelectMenu()
+    
         return (
       
-            // <select id="tag-option" onChange = {this.handleTagChange}>
-            //     <option value = "all">All Tags</option>
-            //     {tagOptions}
-            // </select> 
-            <h1>Here goes the filter</h1>
+            <select id="tag-option" onChange = {this.handleTagChange}>
+                <option value = "all">All Tags</option>
+                {tagOptions}
+            </select> 
    
         )
 }
