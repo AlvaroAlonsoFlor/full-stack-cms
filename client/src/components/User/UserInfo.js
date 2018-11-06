f35fa0a32e6b2e9e1b898606e618825c2008e995
Fofiko <foteinid@gmail>
2018-11-06 18:17:39 +0000

    Refactor UserInfo to redirect after User Delete

--- client/src/components/User/UserInfo.js
+++ client/src/components/User/UserInfo.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import Request from '../../helpers/Request'
 
-const UserInfo = (props) => {
-    let userEditUrl = '/users/' + props.user.id + '/edit';
-    let userDeleteUrl = '/users/' + props.user.id
-
-    function handleDelete() {
-        // console.log(handleDelete)
-        const request = new Request()
-        request.delete(userDeleteUrl)
-        
+class UserInfo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            redirectNow: false
+        }    
+    this.handleDelete = this.handleDelete.bind(this);
     }
 
-    return (
 
-        <div className="user-info">
-            <h4 className="user-name">{props.user.name}</h4>
-            <Link to={userEditUrl}>Edit User</Link>
-            <button type="button" onClick={handleDelete}>Delete User</button>
-        </div>
-    );
+    handleDelete() {
+        let userDeleteUrl = '/users/' + this.props.user.id
+        const request = new Request()
+        request.delete(userDeleteUrl)
+            .then(() => this.setState({redirectNow: true}))
+    }
+
+    handleRedirect() {
+        console.log("top of method")
+        if (this.state.redirectNow) {
+            console.log("inside of method")
+            return <Redirect to='/'/>
+        }
+    }
+
+    render() {
+        let userEditUrl = '/users/' + this.props.user.id + '/edit';
+        return (
+            <div className="user-info">
+                <h4 className="user-name">{this.props.user.name}</h4>
+                <Link to={userEditUrl}>Edit User</Link>
+                <button type="button" onClick={this.handleDelete}>Delete User</button>
+                {this.handleRedirect()}
+            </div>
+        );
+    }
+
 }
 
 
