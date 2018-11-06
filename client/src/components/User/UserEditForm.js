b459e4af8a042692cf74fef478314ea08f970a33
Fofiko <foteinid@gmail>
2018-11-06 19:18:02 +0000

    Complete UserEdit Form

--- client/src/components/User/UserEditForm.js
+++ client/src/components/User/UserEditForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Request from '../../helpers/Request';
 import { Redirect } from 'react-router-dom';
 
@@ -10,12 +10,57 @@ export default class UserEditForm extends Component {
             password: this.props.location.state.user.password,
             redirectNow: false
         }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleNameChange(event) {
+        this.setState({ name: event.target.value })
+    }
+
+    handlePasswordChange(event) {
+        this.setState({ password: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const name = this.state.name
+        const password = this.state.password
+        const id = this.props.location.state.user.id
+        const request = new Request()
+        request.patch(`/users/${id}`, {
+            "name": name,
+            "password": password
+        })
+            .then(() => this.setState({ redirectNow: true }))
+    }
+
+    handleRedirect() {
+        const userId = this.props.location.state.user.id
+        if (this.state.redirectNow) {
+
+            return <Redirect to={`/users/${userId}`} />
+        }
+        return null
+
+    }
+
+
     render() {
-        console.log(this.state.name)
         return (
-            <h1>YOLO</h1>
-        )
+            <div>
+                <form className='edit-article-form' onSubmit={this.handleSubmit}>
+                    <label>Name</label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} />
+                    <label>Password</label>
+                    <input type="text" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                
+                    <button type="submit">Edit user</button>
+                    {this.handleRedirect()}
+                </form>
+
+            </div>
+        );
     }
 }
