3c53034886d26c2e54336c04777cabf6334bc379
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-06 11:05:22 +0000

    Create handle change for each field of the form

--- client/src/components/User/UserArticleForm.js
+++ client/src/components/User/UserArticleForm.js
@@ -8,28 +8,41 @@ export default class UserArticleForm extends Component {
         this.state = {
 
         }
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleLeadChange = this.handleLeadChange.bind(this);
+        this.handleBodyChange = this.handleBodyChange.bind(this);
+        
+    }
+
+    handleTitleChange() {
+
+    }
+
+    handleLeadChange() {
+
+    }
+
+    handleBodyChange() {
+
     }
 
     render() {
-        console.log(this.props.location.state);
         const user = this.props.location.state.user.user;
         const article = this.props.location.state.article.article;
 
-        console.log('user', user);
-        console.log('article', article);
 
         //we need to pass same name, and date, check if we update, patch request should handle it
         //remember onSubmit on form
 
         return(
             <div>
-                <form>
+                <form className='edit-article-form'>
                     <label>Title</label>
-                    <input type="text" name="title" value={article.title}/>
+                    <input type="text" name="title" value={article.title} onChange={this.handleTitleChange}/>
                     <label>Lead</label>
-                    <input type="text" name="lead" value={article.lead}/>
+                    <input type="text" name="lead" value={article.lead} onChange={this.handleLeadChange}/>
                     <label>Body</label>
-                    <input type="text" name="body" value={article.body}/>
+                    <textarea type="text" name="body" value={article.body} onChange={this.handleBodyChange}/>
                     
                     <button type="submit">Edit article</button>
                 
