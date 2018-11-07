3b2a22b2599a74e5c3b4ac77c589a24983200bb9
Fofiko <foteinid@gmail>
2018-11-07 19:47:45 +0000

    Refactor User Articles Container to bloomer

--- client/src/containers/Users/UserArticlesContainer.js
+++ client/src/containers/Users/UserArticlesContainer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import UserArticlePreview from '../../components/User/UserArticlePreview';
 import { Link } from 'react-router-dom';
 import UserArticleFilter from '../../components/User/UserArticleFilter';
+import { Container } from 'bloomer/lib/layout/Container';
+import { Button } from 'bloomer/lib/elements/Button';
+import { Section } from 'bloomer/lib/layout/Section';
 
 
 const UserArticlesContainer = ({user, onFilter, articlesFiltered, onDelete}) => {
@@ -15,18 +18,26 @@ const UserArticlesContainer = ({user, onFilter, articlesFiltered, onDelete}) => 
     
     return (
         <div>
-            <Link to={{
-          pathname: `${user.id}/articles/new`,
-          state: {
-              user: {user}
-          }
-          }}>Write a new article</Link>
-             <UserArticleFilter articles={articles} onFilter={onFilter}/>
-             <UserArticlePreview onDelete={onDelete} articles={articlesFiltered} user={user} />
+            <Container>
+                <Section>
+                    <Button>
+                        <Link to={{
+                            pathname: `${user.id}/articles/new`,
+                            state: {
+                                user: { user }
+                            }
+                        }}>Write a new article</Link>
+                    </Button>
+                </Section>
+                <Section>
+                    <UserArticleFilter articles={articles} onFilter={onFilter} />
+                </Section>
+                <Section>
+                    <UserArticlePreview onDelete={onDelete} articles={articlesFiltered} user={user} />
+                </Section>
+                
+            </Container>
         </div>
-       
-
-    
     );
 }
 
