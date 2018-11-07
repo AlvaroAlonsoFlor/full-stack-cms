7f1f121c51a1459206f266b56067b7eac6bddfd6
Fofiko <foteinid@gmail>
2018-11-07 19:15:27 +0000

    User Container conflict

--- client/src/containers/Users/UserContainer.js
+++ client/src/containers/Users/UserContainer.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import UserNavBar from '../../components/User/UserNavBar';
 import UserArticlesContainer from './UserArticlesContainer';
 import UserInfo from '../../components/User/UserInfo';
+import Request from '../../helpers/Request';
 
 
 export default class UserContainer extends Component {
@@ -15,43 +16,59 @@ export default class UserContainer extends Component {
         };
 
         this.handleTagFilter = this.handleTagFilter.bind(this);
+        this.handleArticleDelete = this.handleArticleDelete.bind(this);
+    }
+
+    handleArticleDelete() {
+        console.log('here handle delete');
+        const request = new Request();
+        request.get(`/users/${this.state.user.id}` + '?projection=embedArticle')
+            .then((data) => {
+                this.setState({ user: data, filteredArticles: data.articles })
+            })
+
+
     }
 
     handleTagFilter(filterName) {
         if (filterName === 'all') {
-            this.setState({filteredArticles: this.state.user.articles})
+            this.setState({ filteredArticles: this.state.user.articles })
         } else {
             const filteredArticles = this.state.user.articles.filter((article) => {
                 return article.tag === filterName
             })
 
-            this.setState({filteredArticles: filteredArticles})
+            this.setState({ filteredArticles: filteredArticles })
         }
 
 
-        
+
 
     }
 
     componentDidMount() {
+        console.log('state in mount', this.props.location.state);
         if (this.props.location.state) {
-            this.setState({user: this.props.location.state.user.user})
+            this.setState({ user: this.props.location.state.user.user })
             // optional ?
-            // this.setState({filteredArticles: this.props.location.state.user.user.articles})
+            this.setState({ filteredArticles: this.props.location.state.user.user.articles })
         }
+
+
 
         //loads if it doesn't have the info
         else {
-        let url = '/users/' + this.state.idToRender + '?projection=embedArticle';
-        fetch(url)
-            .then(request => request.json())
-            .then((data) => {
-                this.setState({filteredArticles: data.articles})
-                return this.setState({user: data})})
-            
-            
+            let url = '/users/' + this.state.idToRender + '?projection=embedArticle';
+            fetch(url)
+                .then(request => request.json())
+                .then((data) => {
+                    this.setState({ filteredArticles: data.articles })
+                    return this.setState({ user: data })
+                })
+
+
         }
-        
+
     }
 
     render() {
@@ -64,17 +81,17 @@ export default class UserContainer extends Component {
             articles = this.state.user.articles;
         }
 
-        return(
-            
+        console.log(this.state.filteredArticles);
+
+        return (
+
             <Fragment>
-                <UserNavBar user={this.state.user} /> 
+                <UserNavBar user={this.state.user} />
                 <UserInfo user={this.state.user} />
-                <UserArticlesContainer user={this.state.user} articlesFiltered={articles} onFilter={this.handleTagFilter} /> 
+                <UserArticlesContainer onDelete={this.handleArticleDelete} user={this.state.user} articlesFiltered={articles} onFilter={this.handleTagFilter} />
             </Fragment>
         )
     }
 
 
 }
-
-
