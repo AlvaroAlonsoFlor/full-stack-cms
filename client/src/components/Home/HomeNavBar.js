55171cad8f11ffbd43d680cb70f0ae9b4126090e
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-07 11:10:52 +0000

    Add bloomer to packages and style home nav bar

--- client/src/components/Home/HomeNavBar.js
+++ client/src/components/Home/HomeNavBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+
+import {Navbar} from 'bloomer';
+
+import { NavbarItem } from 'bloomer/lib/components/Navbar/NavbarItem';
+import { NavbarLink } from 'bloomer/lib/components/Navbar/NavbarLink';
+
 
 const HomeNavBar = (props) => {
     
     return(
         <div>
-            <Link to='/'>Home</Link>
-            <Link to='/articles'>Articles</Link>
+            <Navbar style={{backgroundColor: "#6B9080"}}>
+
+                <NavbarItem  style={{color: "white"}}  href='/'>Home</NavbarItem>
+                <NavbarLink style={{color: "white"}} to='/articles'> Articles </NavbarLink>
+                
+            </Navbar>
+                
+            
+            
+        
+            
         </div>
+        
+        
                 
             
     );
