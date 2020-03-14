a7076861cf0e51c3c1339cfdb4af726b192e8dc1
VladmirVolgen <alvaroalonsoflor@gmail.com>
2020-03-14 17:56:56 +0000

    Fix call to deprecated class

--- server/src/main/java/com/codeclan/projects/cms/configurations/SpringDataRestConfig.java
+++ server/src/main/java/com/codeclan/projects/cms/configurations/SpringDataRestConfig.java
@@ -4,12 +4,13 @@ import com.codeclan.projects.cms.models.Article;
 import com.codeclan.projects.cms.models.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import projections.EmbedArticle;
 import projections.EmbedUser;
 
 @Configuration
-    public class SpringDataRestConfig extends RepositoryRestConfigurerAdapter {
+    public class SpringDataRestConfig implements RepositoryRestConfigurer {
 
 
         @Override
