7d80b437d1e4087452290e1e1f6275bb4ecbb210
VladmirVolgen <alvaroalonsoflor@gmail.com>
2020-03-15 10:34:28 +0000

    Refactor hibernate queries to use non deprecated methods

--- server/src/main/java/com/codeclan/projects/cms/repositories/ArticleRepository/ArticleRepositoryImpl.java
+++ server/src/main/java/com/codeclan/projects/cms/repositories/ArticleRepository/ArticleRepositoryImpl.java
@@ -1,18 +1,23 @@
 package com.codeclan.projects.cms.repositories.ArticleRepository;
 
 import com.codeclan.projects.cms.models.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Slf4j
 public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
 
     @Autowired
@@ -25,12 +30,16 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
         Session session = entityManager.unwrap(Session.class);
 
         try{
-            Criteria cr = session.createCriteria(Article.class);
-            cr.add(Restrictions.eq("tag", tag));
-            articles = cr.list();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Article> criteria = criteriaBuilder.createQuery(Article.class);
+            Root<Article> rootArticle = criteria.from(Article.class);
+            criteria.select(rootArticle);
+            criteria.where(criteriaBuilder.equal(rootArticle.get("tag"), tag));
+
+            articles = session.createQuery(criteria).getResultList();
         }
         catch(HibernateException e){
-            e.printStackTrace();
+            log.error("Error getting articles by category", e);
         }
         finally {
             session.close();
@@ -45,11 +54,16 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
         Session session = entityManager.unwrap(Session.class);
 
         try {
-            Criteria cr = session.createCriteria(Article.class);
-            cr.addOrder(Order.desc("date"));
-            articles = cr.list();
+
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Article> criteria = criteriaBuilder.createQuery(Article.class);
+            Root<Article> rootArticle = criteria.from(Article.class);
+            criteriaBuilder.desc(rootArticle.get("date"));
+
+            articles = session.createQuery(criteria).getResultList();
+
         } catch(HibernateException e) {
-            e.printStackTrace();
+            log.error("Error getting articles sorted by date", e);
         } finally {
             session.close();
         }
